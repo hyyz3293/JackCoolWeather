70db12ab5bf3bc1d8ab63ed008c291f0601a0b9e
hyyz3293 <377956638@qq.com>
2018-05-02 09:20:52 +0800

    第一次提交

---
+++ app/src/main/java/jack/com/cn/modle/login/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package jack.com.cn.modle.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import jack.com.cn.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
---
+++ app/src/main/java/jack/com/cn/modle/testipc/IpcTwoActivity.java
@@ -0,0 +1,15 @@
+package jack.com.cn.modle.testipc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import jack.com.cn.R;
+
+public class IpcTwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ipc_two);
+    }
+}
